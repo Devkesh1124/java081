b44797e08d638ea587e838d26855b7b52ef84469
Devkesh <ydevkesh565@gmail.com>
2025-08-27 14:03:40 +0530

    27 Aug 2k25: exception handling in java

---
+++ src/MyException.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MyException {
+    public static void main(String[] args) {
+        int x,y,z=0;
+        Scanner sc=new Scanner(System.in);
+        try{
+            x=sc.nextInt();
+            y=sc.nextInt();
+            z=x/y;
+            System.out.println("EXCEPTION NoT Occurs");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Result is:"+z);
+        }
+    }
+}
